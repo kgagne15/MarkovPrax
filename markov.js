bc5b9569f374194326c160ca5d0fca6b5552f4c8
kgagne15 <kristinagagne@ymail.com>
2022-08-24 07:13:19 -0400

    markov file 'complete'

--- markov.js
+++ markov.js
@@ -17,13 +17,46 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    //creates new map
+    let chains = new Map();
+
+    //for each word in this.words, it will set the next word to either the next word or null
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i]
+      let nextWord = this.words[i + 1] || null;
+      //if chains contains a word, get word and push nextWord
+      if (chains.has(word)) {
+        chains.get(word).push(nextWord);
+      } 
+      //else set word to nextWord
+      else {
+        chains.set(word, [nextWord])
+      }
+      
+    }
+  }
+  //what is static?
+  static choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)];
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    //pick a random key to begin
+    let keys = Array.from(this.chains.keys())
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    //while out.length is less than numWords input and key is not null, push key to out
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+    //returns output with words joined on space
+    return out.join(" ");
   }
 }
+
+module.exports = { MarkovMachine };
